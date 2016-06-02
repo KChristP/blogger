d11d499553d6022f2c2681becf00e33d5f7b63fa
Keenan Phillips <0coolzero6001@gmail.com>
2016-06-02 09:59:36 -0700

    form-based workflow feature completed

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,7 @@
 module ArticlesHelper
+
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end# this method was written so that app/controllers/articles_controller's #create method could use params[:article] as an argument for Article.new but that wasnt allowed
+
 end
