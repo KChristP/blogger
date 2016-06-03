07bf2fca8409a7916bedf257c35b4a1500cf229c
Keenan Phillips <0coolzero6001@gmail.com>
2016-06-03 14:14:45 -0700

    tagging feature completed?

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,7 +1,7 @@
 module ArticlesHelper
 
   def article_params
-    params.require(:article).permit(:title, :body)
+    params.require(:article).permit(:title, :body, :tag_list)
   end# this method was written so that app/controllers/articles_controller's #create method could use params[:article] as an argument for Article.new but that wasnt allowed
 
 end
