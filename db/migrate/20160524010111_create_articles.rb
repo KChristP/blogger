e867508a915471c411cbe86eea85187c081bd2d8
Keenan Phillips <0coolzero6001@gmail.com>
2016-06-11 08:13:57 -0700

    tried to link authors to articles

--- db/migrate/20160524010111_create_articles.rb
+++ db/migrate/20160524010111_create_articles.rb
@@ -3,6 +3,7 @@ class CreateArticles < ActiveRecord::Migration
     create_table :articles do |t|
       t.string :title
       t.text :body
+      t.references :author, index: true, foreign_key: true#trying to add one to many relationship of 1 author and many articles
 
       t.timestamps null: false
     end
